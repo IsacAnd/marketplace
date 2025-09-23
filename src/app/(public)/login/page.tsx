36adc7a14b9438cddc1bb701202500cbbf3f8830
IsacAnd <grimjaeguer@gmail.com>
2025-09-23 08:40:33 -0300

    feat: login and register views implementation

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,3 +1,62 @@
 export default function Login() {
-  return;
+  return (
+    <div className="w-screen h-screen bg-gray-100 flex justify-center items-center ">
+      <div
+        className="w-full max-w-sm flex flex-col gap-4 rounded-lg p-8 text-gray-700 backdrop-blur-sm hover:shadow-lg"
+        style={{ boxShadow: "0px 0px 10px 2px rgba(0,0,0,0.05)" }}
+      >
+        <h2 className="text-center text-2xl font-bold mb-4 text-gray-600">
+          Login
+        </h2>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="email" className="font-medium">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="exemplo@email.com"
+            className="border border-gray-300 rounded-lg p-3 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 outline-none transition-colors duration-200"
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="password" className="font-medium">
+            Senha
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Digite sua senha"
+            className="border border-gray-300 rounded-lg p-3 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 outline-none transition-colors duration-200"
+          />
+        </div>
+        <div className="flex items-center gap-2">
+          <input
+            type="checkbox"
+            id="terms"
+            className="w-4 h-4 text-blue-500 border-gray-300 rounded focus:ring-2 focus:ring-blue-200"
+          />
+          <label htmlFor="terms" className="text-sm">
+            Aceito os termos
+          </label>
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 text-white font-bold p-3 rounded-lg cursor-pointer hover:bg-blue-400 transform transition-all duration-300"
+        >
+          Entrar
+        </button>
+
+        {/* Link de registro */}
+        <a
+          href="/register"
+          className="text-center text-blue-500 hover:underline mt-2 text-sm"
+        >
+          Não possuo uma conta
+        </a>
+      </div>
+    </div>
+  );
 }
--- src/app/(public)/register/page.tsx
+++ src/app/(public)/register/page.tsx
@@ -1,3 +1,98 @@
 export default function Register() {
-  return;
+  return (
+    <div className="w-screen h-screen h-max bg-gray-100 flex justify-center items-center ">
+      <div
+        className="w-full max-w-sm flex flex-col gap-3 rounded-lg px-8 py-4 text-gray-700 backdrop-blur-sm hover:shadow-lg"
+        style={{ boxShadow: "0px 0px 10px 2px rgba(0,0,0,0.05)" }}
+      >
+        <h2 className="text-center text-2xl font-bold mb-4 text-gray-600">
+          Cadastro
+        </h2>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="cpf" className="font-medium">
+            Cpf
+          </label>
+          <input
+            type="number"
+            name="cpf"
+            id="cpf"
+            placeholder="000.000.000-00"
+            className="border border-gray-300 rounded-lg p-3 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 outline-none transition-colors duration-200"
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="completeName" className="font-medium">
+            Nome completo
+          </label>
+          <input
+            type="text"
+            name="completeName"
+            id="completeName"
+            placeholder="Digite seu nome completo aqui"
+            className="border border-gray-300 rounded-lg p-3 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 outline-none transition-colors duration-200"
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="email" className="font-medium">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Digite seu email aqui"
+            className="border border-gray-300 rounded-lg p-3 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 outline-none transition-colors duration-200"
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="phone" className="font-medium">
+            Telefone
+          </label>
+          <input
+            type="tel"
+            name="phone"
+            id="phone"
+            placeholder="(00) 00000-0000"
+            className="border border-gray-300 rounded-lg p-3 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 outline-none transition-colors duration-200"
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="password" className="font-medium">
+            Senha
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Digite sua senha"
+            className="border border-gray-300 rounded-lg p-3 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 outline-none transition-colors duration-200"
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="confirmPassword" className="font-medium">
+            Confirme sua senha
+          </label>
+          <input
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            placeholder="Digite novamente sua senha"
+            className="border border-gray-300 rounded-lg p-3 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 outline-none transition-colors duration-200"
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 text-white font-bold p-3 rounded-lg cursor-pointer hover:bg-blue-400 transform transition-all duration-300"
+        >
+          Cadastrar
+        </button>
+        <a
+          href="/login"
+          className="text-center text-blue-500 hover:underline mt-2 text-sm"
+        >
+          Já possuo uma conta
+        </a>
+      </div>
+    </div>
+  );
 }
